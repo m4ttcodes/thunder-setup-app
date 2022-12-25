38204f0ba0a0ee30c3fd052b713853cfb5315eb0
Vicente Aburto <vicenteaburtog@gmail.com>
2022-12-25 01:27:09 -0300

    Primer commit

---
+++ src/product-data.js
@@ -0,0 +1,136 @@
+const products = [
+  {
+    id: 1,
+    name: "AMD Ryzen 5 5600",
+    productType: "Procesador AMD",
+    price: 159990,
+    rating: 5,
+    image:
+      "https://www.amd.com/system/files/2019-07/238593-ryzen-5G-pib-right-facing-1260x709.png",
+    description:
+      "Procesador AMD Ryzen 5 5600. Muy bueno para armar tu primera PC gamer. Funciona para stremear y jugar a la vez.",
+  },
+  {
+    id: 2,
+    name: "Intel i7 10700",
+    productType: "Procesador Intel",
+    price: 300000,
+    rating: 4,
+    image:
+      "https://www.rosetech.com.bd/wp-content/uploads/2021/01/i7-10700_compressed.jpg",
+    description:
+      "Procesador Intel i7 10700. Ideal para creación de contenido y hacer un upgrade a tu PC.",
+  },
+  {
+    id: 3,
+    name: "NVIDIA RTX 2060",
+    productType: "GPU NVIDIA",
+    price: 299990,
+    rating: 4,
+    image:
+      "https://i.ebayimg.com/00/s/NTg2WDgwMA==/z/03MAAOSwCQ1f7Qny/$_86.PNG",
+    description:
+      "Tarjeta de video NVIDIA RTX 2060. Ideal para jugar a todo.",
+  },
+  {
+    id: 4,
+    name: "NVIDIA RTX 3050",
+    productType: "GPU NVIDIA",
+    price: 350000,
+    rating: 5,
+    image:
+      "https://hd2.tudocdn.net/951893?w=1200",
+    description:
+      "Tarjeta de video NVIDIA RTX 3050. Versión mas económica y potente para disfrutar de la edición y videojuegos.",
+  },
+  {
+    id: 5,
+    name: "Gigabyte B550M DS3H",
+    productType: "Placa madre",
+    price: 119990,
+    rating: 5,
+    image:
+      "https://publicapi.solotodo.com/products/83611/picture/?width=1000&height=520",
+    description:
+      "Placa madre Gigabyte B550. Modelo de gama media ideal para montar un PC versátil. Solo compatible con AMD.",
+  },
+  {
+    id: 6,
+    name: "MSI B560M-A PRO",
+    productType: "Placa madre",
+    price: 99990,
+    rating: 4,
+    image:
+      "https://publicapi.solotodo.com/products/122447/picture/?width=1000&height=520",
+    description:
+      "Placa madre MSI B560M-A PRO. Modelo compatible con procesadores Intel.",
+  },
+  {
+    id: 7,
+    name: "Kingston Fury Beast",
+    productType: "Memoria RAM",
+    price: 29990,
+    rating: 5,
+    image:
+      "https://publicapi.solotodo.com/products/116951/picture/?width=1000&height=520",
+    description:
+      "Memoria RAM Kingston Fury. Modelo con trayectoria marcado por su excelente calidad/precio.",
+  },
+  {
+    id: 8,
+    name: "Corsair Vengeance RGB Pro",
+    productType: "Memoria RAM",
+    price: 90000,
+    rating: 5,
+    image:
+      "https://publicapi.solotodo.com/products/76876/picture/?width=1000&height=520",
+    description:
+      "Pack dual-channel para sacar el máximo rendimiento al uso de tu PC.",
+  },
+  {
+    id: 9,
+    name: "EVGA 600W",
+    productType: "Fuente de poder",
+    price: 39990,
+    rating: 3,
+    image:
+      "https://publicapi.solotodo.com/products/22103/picture/?width=1000&height=520",
+    description:
+      "Fuente de poder EVGA 600W. Para comenzar tu build es una opción recomendable.",
+  },
+  {
+    id: 10,
+    name: "NZXT H510",
+    productType: "Gabinetes",
+    price: 59990,
+    rating: 4,
+    image:
+      "https://publicapi.solotodo.com/products/78230/picture/?width=1000&height=520",
+    description:
+      "Gabinete tamaño mediano.",
+  },
+  {
+    id: 11,
+    name: "Lian Li Lancool III",
+    productType: "Gabinetes",
+    price: 169990,
+    rating: 5,
+    image:
+      "https://publicapi.solotodo.com/products/154769/picture/?width=1000&height=520",
+    description:
+      "Gabinete gama alta con las mejores prestaciones del mercado para mantener tu PC a un 100%.",
+  },
+  {
+    id: 12,
+    name: "Western Digital Black",
+    productType: "M.2",
+    price: 119990,
+    rating: 5,
+    image:
+      "https://publicapi.solotodo.com/products/135906/picture/?width=1000&height=520",
+    description:
+      "Memoria M.2 para almacenar todos tus datos.",
+  },
+];
+
+export default products;
